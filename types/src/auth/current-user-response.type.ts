7484eb604f6b0a48d2589da4afc2c8372ec8c56c
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-05-08 16:49:41 +0200

    feat(types): include tenant in current user response

--- types/src/auth/current-user-response.type.ts
+++ types/src/auth/current-user-response.type.ts
@@ -6,6 +6,9 @@ export interface CurrentUserResponseDto {
   name: string
   email: string
   image?: string
+  tenantId: string
+  tenantName: string
+  tenantSlug: string
   roles: string[]
   permissions: Partial<Record<Resource, Permission[]>>
 }
